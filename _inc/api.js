2e469e5fc8f5621f4dd54267f9ef41c44f2e0ae7
Mark <markk/9964@gmail.com>
2019-02-01 16:11:17 +0200

    style api

--- _inc/api.js
+++ _inc/api.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+jQuery(document).ready(function ($) {
 
     $('#zipCode').keyup(function () {
         var select = $('#school-district');
@@ -9,7 +9,6 @@ $(document).ready(function () {
             dataType: 'JSON'
         }).done(function (data) {
            if(data.length > 0){
-               console.log(data);
 
                select.html('');
                select.append('<option value="-">select</option>');
@@ -37,7 +36,6 @@ $(document).ready(function () {
             dataType: 'JSON'
         }).done(function (data) {
             if(data.length > 0){
-                console.log(data);
 
                 select.html('');
                 select.append('<option value="-">select school</option>');
@@ -59,7 +57,6 @@ $(document).ready(function () {
             dataType: 'JSON'
         }).done(function (data) {
             if(data.length > 0){
-                console.log(data);
 
                 select.html('');
                 select.append('<option value="-">select grade</option>');
@@ -90,17 +87,18 @@ $(document).ready(function () {
             data: data,
             dataType: 'JSON'
         }).done(function (data) {
+            console.log(data);
             for(var i=0; i<data.length; i++)
             {
-                var btn = getBuyButton(data[i].plan_type_id);
                 $('#content_step_2').append('' +
-                    '<div class="col-sm-12">'+
-                    '<header><h4>'+data[i].web_display_header+'</h4></header>'+
+                    '<div class="col-sm-12" data-webplan-id="'+data[i].plan_type_id+'">'+
+                    '<header><b>'+data[i].web_display_header+'</b></header>'+
                     data[i].web_display_collateral+
                     '<div class="butt">' +
                     '' +
                     '</div>'+
-                    '</div>')
+                    '</div>');
+                getBuyButton(data[i].plan_type_id);
             }
         });
         $(this).parent().removeClass('active show');
@@ -110,6 +108,7 @@ $(document).ready(function () {
     });
 
     function getBuyButton(plan_id) {
+
         var main = $('#school-district option:selected');
         var data = {
             type: 'planprem',
@@ -118,13 +117,47 @@ $(document).ready(function () {
             lowestGradeLevel: main.attr('data-lowest'),
             highestGradeLevel: main.attr('data-highest')
         };
+
         $.ajax({
             method: 'GET',
-            url: '/ajax.php',
+            url: './ajax.php',
             data: data,
             dataType: 'JSON'
         }).done(function (data) {
-           return data;
+            console.log(data);
+            if(data[0].low_option_premium != null){
+                console.log('low_option_premium');
+                $('*[data-webplan-id="'+data[0].plan_type_id+'"] .butt').append('' +
+                    '<div class="but-block">'+
+                        '<b>$'+data[0].low_option_premium+' Base Option</b>'+
+                        '<p>Best price, lowest covarege</p>'+
+                        '<a href="#" class="btn btn-success" data-price="'+data[0].low_option_premium+'">Add to Cart</a>' +
+                    '</div>'+
+                    '<div class="but-block">'+
+                        '<b>$'+data[0].mid_option_premium+' Mid Option</b>'+
+                        '<p>Great price - Great covrage</p>'+
+                        '<a href="#" class="btn btn-success" data-price="'+data[0].mid_option_premium+'">Add to Cart</a>' +
+                    '</div>'+
+                    '<div class="but-block">'+
+                        '<b>$'+data[0].high_option_premium+' High Option</b>'+
+                        '<p>Best Covarage - Higher cost</p>'+
+                        '<a href="#" class="btn btn-success" data-price="'+data[0].high_option_premium+'">Add to Cart</a>' +
+                    '</div>');
+            }else if(data[0].initial_option_premium != null){
+                console.log('initial_option_premium');
+                $('*[data-webplan-id="'+data[0].plan_type_id+'"] .butt').append('' +
+                    '<a href="#" class="btn btn-success">'+data[0].initial_option_premium+'</a>',
+                    '<a href="#" class="btn btn-success">'+data[0].bimonthly_option_premium+'</a>'
+                );
+            }else if(data[0].standard_premium != null){
+                console.log('standard_premium');
+                $('*[data-webplan-id="'+data[0].plan_type_id+'"] .butt').append('' +
+                    '<a href="#" class="btn btn-success">'+data[0].standard_premium+'</a>');
+            }
+            var p_h = $('*[data-webplan-id="' +data[0].plan_type_id+'"] p:nth-child(2)').html();
+            $('*[data-webplan-id="'+data[0].plan_type_id+'"] header').append('<p>'+p_h+'</p>');
+            p_h.html(' ');
+
         });
     }
 
